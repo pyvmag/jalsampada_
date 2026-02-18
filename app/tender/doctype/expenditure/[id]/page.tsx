c737b35da795b2e47a354e1fbe3a611937cdcf2e
pyvmag <140055356+pyvmag@users.noreply.github.com>
2026-02-18 17:50:28 +0530

    HDT-26-01469,

--- app/tender/doctype/expenditure/[id]/page.tsx
+++ app/tender/doctype/expenditure/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import * as React from "react";
 import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
--- app/tender/doctype/expenditure/new/page.tsx
+++ app/tender/doctype/expenditure/new/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import * as React from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
